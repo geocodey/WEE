61d46813b9b59f2ba913a0de0380ce3a57e31fa2
geocodey <feier.marius.andrei@gmail.com>
2019-04-15 14:41:41 +0300

    [dev] Added ButterKnife onClick listener

--- app/src/main/java/com/example/wmseasyexpert/HomeActivity.java
+++ app/src/main/java/com/example/wmseasyexpert/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.wmseasyexpert;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.wmseasyexpert.Models.Example;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,19 +12,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    @OnClick(R.id.fab) void fabClickListener(View v) {
+        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
-        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        fab.setOnClickListener(v-> Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
     }
 
     @Override
