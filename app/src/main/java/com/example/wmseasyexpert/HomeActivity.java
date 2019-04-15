8d41f5b28ccedb34557291f7d710f641cadcae89
geocodey <feier.marius.andrei@gmail.com>
2019-04-15 14:19:09 +0300

    [dev] Added ButterKnife library and migrated to androidx

--- app/src/main/java/com/example/wmseasyexpert/HomeActivity.java
+++ app/src/main/java/com/example/wmseasyexpert/HomeActivity.java
@@ -1,24 +1,27 @@
 package com.example.wmseasyexpert;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.example.wmseasyexpert.Models.Example;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+    @BindView(R.id.fab) FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(v-> Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
     }
