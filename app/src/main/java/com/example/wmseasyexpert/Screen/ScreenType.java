ff7702b6d4510af16696b0c14039c406c448b760
geocodey <feier.marius.andrei@gmail.com>
2019-04-22 11:09:03 +0300

    [dev] added screen types

--- app/src/main/java/com/example/wmseasyexpert/Screen/ScreenType.java
+++ app/src/main/java/com/example/wmseasyexpert/Screen/ScreenType.java
@@ -2,4 +2,7 @@ package com.example.wmseasyexpert.Screen;
 
 public abstract class ScreenType {
     public static final String OPTIONS = "options";
+    public static final String INPUT   = "input";
+    public static final String INFO    = "info";
+    public static final String MENU    = "options";
 }
