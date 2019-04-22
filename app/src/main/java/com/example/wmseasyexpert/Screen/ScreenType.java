20e6f72c3aeecd427d86357176d3670e4d09050e
geocodey <feier.marius.andrei@gmail.com>
2019-04-22 11:52:04 +0300

    [dev] fixed menu label

--- app/src/main/java/com/example/wmseasyexpert/Screen/ScreenType.java
+++ app/src/main/java/com/example/wmseasyexpert/Screen/ScreenType.java
@@ -4,5 +4,5 @@ public abstract class ScreenType {
     public static final String OPTIONS = "options";
     public static final String INPUT   = "input";
     public static final String INFO    = "info";
-    public static final String MENU    = "options";
+    public static final String MENU    = "menu";
 }
