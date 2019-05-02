7a1ecc4467528eaf024d37e7a8bd066707945cdf
geocodey <feier.marius.andrei@gmail.com>
2019-05-02 16:34:08 +0300

    [dev] Added footer message and check button

--- app/src/main/java/com/example/wmseasyexpert/mvp/views/OptionsScreenActivity.java
+++ app/src/main/java/com/example/wmseasyexpert/mvp/views/OptionsScreenActivity.java
@@ -1,12 +1,16 @@
 package com.example.wmseasyexpert.mvp.views;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wmseasyexpert.R;
@@ -17,6 +21,7 @@ import com.example.wmseasyexpert.utils.Toolbar;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,10 +31,13 @@ public class OptionsScreenActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.options_list)
-    ListView optionsList ;
+    ListView optionsList;
+    @BindView(R.id.help_button)
+    Button helpButton;
 
 
     OptionsScreenData screenData;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +51,38 @@ public class OptionsScreenActivity extends AppCompatActivity {
     private void initView() {
         screenData = getScreenData();
         initToolbar();
+        initFooter();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, getListOfOptions());
         optionsList.setAdapter(adapter);
+
         Log.d(TAG, String.valueOf(screenData));
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 
     private List<String> getListOfOptions() {
         List<String> options = new ArrayList<>();
-        for(Option option : screenData.getOptions()){
+        for (Option option : screenData.getOptions()) {
             options.add(option.getText());
         }
         return options;
     }
 
     private void initToolbar() {
-
         toolbar.setTitle(screenData.getTitle() + "(" + screenData.getScreenTag().getId() + ")");
         toolbar.setOnBackClickListener(this::previousScreen);
+    }
+
+    private void initFooter() {
+        String helpKey = screenData.getHelpTag().getHelpKey();
+        String helpMessage = TextUtils.join("",screenData.getHelpTag().getHelpLines());
+        helpButton.setText(helpKey);
+        helpButton.setOnClickListener(v -> alertDialog.show());
+        alertDialog = new AlertDialog.Builder(OptionsScreenActivity.this).create();
+        alertDialog.setTitle("Help");
+        alertDialog.setMessage(helpMessage);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                (dialog, which) -> dialog.dismiss());
     }
 
     private void previousScreen() {
