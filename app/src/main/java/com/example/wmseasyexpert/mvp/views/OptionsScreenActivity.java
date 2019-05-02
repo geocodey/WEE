0ee3b9f9c0d44045ad788d4581a11078ef4c7044
geocodey <feier.marius.andrei@gmail.com>
2019-05-02 15:49:42 +0300

    [dev] Footer section added for options screen.

--- app/src/main/java/com/example/wmseasyexpert/mvp/views/OptionsScreenActivity.java
+++ app/src/main/java/com/example/wmseasyexpert/mvp/views/OptionsScreenActivity.java
@@ -1,17 +1,21 @@
 package com.example.wmseasyexpert.mvp.views;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wmseasyexpert.R;
+import com.example.wmseasyexpert.models.screen.Option;
 import com.example.wmseasyexpert.models.screen.OptionsScreenData;
 import com.example.wmseasyexpert.utils.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
@@ -21,6 +25,9 @@ public class OptionsScreenActivity extends AppCompatActivity {
     private static String TAG = OptionsScreenActivity.class.getName();
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.options_list)
+    ListView optionsList ;
+
 
     OptionsScreenData screenData;
 
@@ -36,13 +43,24 @@ public class OptionsScreenActivity extends AppCompatActivity {
     private void initView() {
         screenData = getScreenData();
         initToolbar();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, getListOfOptions());
+        optionsList.setAdapter(adapter);
         Log.d(TAG, String.valueOf(screenData));
-        Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+    }
+
+    private List<String> getListOfOptions() {
+        List<String> options = new ArrayList<>();
+        for(Option option : screenData.getOptions()){
+            options.add(option.getText());
+        }
+        return options;
     }
 
     private void initToolbar() {
 
-        toolbar.setTitle(screenData.getTitle() + "(" + screenData.getScreenTag().getId()+ ")");
+        toolbar.setTitle(screenData.getTitle() + "(" + screenData.getScreenTag().getId() + ")");
         toolbar.setOnBackClickListener(this::previousScreen);
     }
 
